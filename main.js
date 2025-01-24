dcd220c26dfc47fe5eab7849726ed1c239c5cf5e
Kubat <Kubatb990@gmail.com>
2025-01-24 14:35:58 +0300

    save hw 20

---
+++ main.js
@@ -0,0 +1,60 @@
+//! task-1 hw-20
+// let student1 = {
+//   name: "Josh",
+//   lang: "KG",
+//   age: 24,
+// };
+// let student2 = student1;
+// student2 = Object.assign({}, student1);
+// delete student2.age;
+// console.log(student1);
+// console.log(student2);
+//! task-2
+// let array = [
+//   1,
+//   true,
+//   3,
+//   { name: "ZH", hobbies: ["swimming", "reading"] },
+//   5,
+//   "hello",
+//   "hi",
+// ];
+//? array-1
+// console.log(array[1]);
+//? array-2
+// console.log(array[4]);
+//? array-3
+// console.log(array[3].hobbies);
+//? array-4
+// console.log(array[3].hobbies.pop());
+//? array-5
+// array.unshift("Kubat")
+// console.log(array);
+//? array-6
+// array.push("Kubat")
+// console.log(array);
+//? array-7
+// array.shift()
+// console.log(array);
+//? array-8
+// console.log(array.indexOf("hello"));
+//? array-9
+// console.log(array.slice(0, 3));
+//? array-10
+// array[3].hobbies.push("Football")
+// console.log(array);
+//? array-12
+// let length = array.length;
+// console.log(length);
+//! task-3
+// const array = [1, 2, 3, "sdf", 4, "sdf", {}, 3, true, 2, [1, 6, 3], 1];
+// function sumOfNumbers(array2) {
+//   let sum = 0;
+//   for (let i = 0; i < array2.length; i++) {
+//     if (typeof array2[i] === "number") {
+//       sum += array2[i];
+//     }
+//   }
+//   return sum;
+// }
+// console.log(sumOfNumbers(array));
